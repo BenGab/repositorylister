29964155deb1b09f0714047ce1c9fe080ae35528
gaborbenko <benkogabor@msn.com>
2017-01-22 13:28:00 +0100

    vue PoC frist component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <hello></hello>
+    <repositories></repositories>
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello'
+import Repositories from './components/Repositories'
 
 export default {
   name: 'app',
   components: {
-    Hello
+    Repositories
   }
 }
 </script>
---
+++ src/components/Repositories.vue
@@ -0,0 +1,15 @@
+<template>
+	<div class="repositories">
+		<h1>{{title}}</h1>
+	</div>
+</template>
+<script>
+    export default {
+        name: 'repositories',
+        data() {
+            return {
+                title: 'Repository list of Addy Osmani'
+            }
+        }
+    }
+</script>
