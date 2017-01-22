1b13f19a16c79a3ccbfc9201cd0a0f593cfa1d1f
gaborbenko <benkogabor@msn.com>
2017-01-22 18:13:35 +0100

    created repository list from git api

--- src/components/Repositories.vue
+++ src/components/Repositories.vue
@@ -1,6 +1,19 @@
 <template>
 	<div class="repositories">
 		<h1>{{title}}</h1>
+        <div class="container">
+            <div class="row" v-for="repositori in repositories">
+                <div class="col-md-4">
+                    {{repositori.name}}
+                </div>
+                <div class="col-md-4">
+                    {{repositori.description}}
+                </div>
+                <div class="col-md-2">
+                    <a v-bind:href="repositori.html_url" target="blank">Git url</a>
+                </div>
+            </div>
+        </div>
 	</div>
 </template>
 <script>
@@ -8,12 +21,14 @@
         name: 'repositories',
         data() {
             return {
-                title: 'Repository list of Addy Osmani'
+                title: 'Repository list of Addy Osmani',
+                repositories: []
             }
         },
         created: function () {
             this.$http.get('https://api.github.com/users/addyosmani/repos')
                 .then(function(response) {
+                    this.repositories = response.data
                     console.log(response.body)
                 })
 
